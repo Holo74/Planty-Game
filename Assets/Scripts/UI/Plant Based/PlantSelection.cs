5c899b8de9c25fef9e7d7b0f87ede6e282171f62
N K <njk0074@gmail.com>
2018-09-29 14:59:48 -0600

    Hover Over Boxes Availible

--- Assets/Scripts/UI/Plant Based/PlantSelection.cs
+++ Assets/Scripts/UI/Plant Based/PlantSelection.cs
@@ -9,17 +9,70 @@ namespace Holo74.Hud
 	{
 		[SerializeField]
 		private LayerMask planterBoxs;
+		[SerializeField]
+		private SpriteRenderer spriteSelection;
+		private PlanterBoxUI mainSelection;
+		[SerializeField]
+		private int box;
+		[SerializeField]
+		private Sprite nonHoverSprite, hoverSpriteCanSelect, hoverSpriteCantSelect;
+		[SerializeField]
+		private bool canSelect = false;
+
+		private void Awake()
+		{
+			mainSelection = transform.parent.GetComponent<PlanterBoxUI>();
+		}
+
 		void Update()
 		{
 			DetectingMouse();
 		}
 
+		private RaycastHit hit;
+		private bool hoverLock, hoverSpriteLock;
 		private void DetectingMouse()
 		{
-			if (Physics.Raycast(InputManager.mouseRay, 200f, planterBoxs))
+			if (Physics.Raycast(InputManager.mouseRay, out hit, 200f, planterBoxs))
 			{
-				Debug.Log("Found");
+				if(hit.transform.gameObject == gameObject)
+				{
+					if (!hoverSpriteLock)
+					{
+						hoverSpriteLock = true;
+						if (canSelect)
+						{
+							mainSelection.mainBox = box;
+							spriteSelection.sprite = hoverSpriteCanSelect;
+						}
+						else
+						{
+							spriteSelection.sprite = hoverSpriteCantSelect;
+						}
+						hoverLock = false;
+					}
+				}
 			}
+			else
+			{
+				if (!hoverLock)
+				{
+					hoverLock = true;
+					hoverSpriteLock = false;
+					mainSelection.mainBox = -1;
+					spriteSelection.sprite = nonHoverSprite;
+				}
+			}
+		}
+
+		public void ActivateUI()
+		{
+			spriteSelection.gameObject.SetActive(true);
+		}
+
+		public void DeactivateUI()
+		{
+			spriteSelection.gameObject.SetActive(false);
 		}
 	}
 
